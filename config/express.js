4fc1209f1907a933f737a0626ff34353f69de8f8
Denis Ogarenko <ogarenko.denis.ru@gmail.com>
2023-12-13 06:12:25 -0500

    modified:   config/express.js

--- config/express.js
+++ config/express.js
@@ -1,46 +1,50 @@
-//Import necessary Node.js modules and Express middleware
-var express = require('express'), 
-	morgan = require('morgan'), 
-	compress = require('compression'), 
-	bodyParser = require('body-parser'), 
-	methodOverride = require('method-override'), 
-	session = require('express-session'); 
+// Import necessary Node.js modules and Express middleware
+var express = require('express'),
+    morgan = require('morgan'),
+    compress = require('compression'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    session = require('express-session');
 
-var config = require('./env/development')
+var config = require('./env/development');
 
-//create Express app and export it
+// Create Express app and export it
 module.exports = function() {
+    var app = express();
 
-	var app = express();
-	if(process.env.NODE_ENV === 'development') {
-		app.use(morgan('dev'));
-	} else if(process.env.NODE_ENV === 'production') {
-		app.use(compress());
-	}
-	app.use(bodyParser.urlencoded({
-		extended: true
-	}));
-	app.use(bodyParser.json());
-	app.use(methodOverride());
-	app.use(session({
-		saveUninitialized : true,
-		resave : true,
-		secret: config.sessionSecret
-	}));
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'));
+    } else if (process.env.NODE_ENV === 'production') {
+        app.use(compress());
+    }
 
-	//Set the directory where views are present and view engine to EJS
-	app.set('views', './app/views');
-	app.set('view engine', 'ejs');
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: config.sessionSecret
+    }));
 
-	//configure route handlers 
-	require('../app/routes/homeRoutes.js')(app); 
-	require('../app/routes/aboutMeRoutes.js')(app); 
-	require('../app/routes/projectsRoutes.js')(app); 
-	require('../app/routes/servicesRoutes.js')(app); 
-	require('../app/routes/contactMeRoutes.js')(app); 
+    // Set the directory where views are present and view engine to EJS
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
 
-	//Use static files
-	app.use(express.static('./public'));
+    // Configure route handlers
+    require('../app/routes/homeRoutes.js')(app);
+    require('../app/routes/aboutMeRoutes.js')(app);
+    require('../app/routes/projectsRoutes.js')(app);
+    require('../app/routes/servicesRoutes.js')(app);
+    require('../app/routes/contactMeRoutes.js')(app);
 
-	return app;
-}
+    // Use static files with caching
+    const oneDay = 24 * 60 * 60 * 1000; // Cache for 24 hours
+    app.use(express.static('./public', {
+        maxAge: oneDay
+    }));
+
+    return app;
+};
