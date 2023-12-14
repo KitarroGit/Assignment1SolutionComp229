a0e215edafca574b2a21bb72a7214b13c756b9f4
Denis Ogarenko <ogarenko.denis.ru@gmail.com>
2023-12-14 14:22:27 -0500

    modified:   app/partials/navigationBar.ejs 	modified:   app/views/home.ejs 	modified:   config/express.js 	modified:   package.json 	modified:   public/yourcssgoeshere/style.css

--- config/express.js
+++ config/express.js
@@ -1,26 +1,29 @@
 // Import necessary Node.js modules and Express middleware
-var express = require('express'),
-    morgan = require('morgan'),
-    compress = require('compression'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    session = require('express-session');
+const express = require('express'),
+      morgan = require('morgan'),
+      compress = require('compression'),
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override'),
+      session = require('express-session');
 
-var config = require('./env/development');
+const config = require('./env/development');
 
 // Create Express app and export it
 module.exports = function() {
-    var app = express();
+    const app = express();
 
     if (process.env.NODE_ENV === 'development') {
+        // In development, use morgan for logging and disable static file caching
         app.use(morgan('dev'));
+        app.use(express.static('./public', { maxAge: 0 }));
     } else if (process.env.NODE_ENV === 'production') {
+        // In production, use compression and enable static file caching
         app.use(compress());
+        const oneDay = 24 * 60 * 60 * 1000; // Cache for 24 hours
+        app.use(express.static('./public', { maxAge: oneDay }));
     }
 
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(methodOverride());
     app.use(session({
@@ -39,12 +42,6 @@ module.exports = function() {
     require('../app/routes/projectsRoutes.js')(app);
     require('../app/routes/servicesRoutes.js')(app);
     require('../app/routes/contactMeRoutes.js')(app);
-
-    // Use static files with caching
-    const oneDay = 24 * 60 * 60 * 1000; // Cache for 24 hours
-    app.use(express.static('./public', {
-        maxAge: oneDay
-    }));
 
     return app;
 };
